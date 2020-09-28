a4f4ed1c643e445f20f6c6c42ba1068cb6259eed
Dione Wilson <dionew1@gmail.com>
2020-09-28 17:15:53 -0600

    Add CommentDetail component

--- src/index.js
+++ src/index.js
@@ -1,52 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import faker from "faker";
+import CommentDetail from "./CommentDetail";
 
 const App = () => {
   return (
     <div className="ui container comments">
-      <div className="comment">
-        <a href="/" className="avatar">
-          <img src={faker.image.avatar()} alt="avatar" />
-        </a>
-        <div className="content">
-          <a href="/" className="author">
-            Sam
-          </a>
-          <div className="metadata">
-            <span className="date">Today at 6:00pm</span>
-          </div>
-          <div className="text">Nice blog Post!</div>
-        </div>
-      </div>
-      <div className="comment">
-        <a href="/" className="avatar">
-          <img src={faker.image.avatar()} alt="avatar" />
-        </a>
-        <div className="content">
-          <a href="/" className="author">
-            Scottie
-          </a>
-          <div className="metadata">
-            <span className="date">Today at 7:00pm</span>
-          </div>
-          <div className="text">Loved it!</div>
-        </div>
-      </div>
-      <div className="comment">
-        <a href="/" className="avatar">
-          <img src={faker.image.avatar()} alt="avatar" />
-        </a>
-        <div className="content">
-          <a href="/" className="author">
-            TJ
-          </a>
-          <div className="metadata">
-            <span className="date">Today at 8:00pm</span>
-          </div>
-          <div className="text">Wonderful!</div>
-        </div>
-      </div>
+      <CommentDetail />
+      <CommentDetail />
+      <CommentDetail />
     </div>
   );
 };
