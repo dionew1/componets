4b8b225296825c5af5935a880dd7bac8b3f7bd60
Dione Wilson <dionew1@gmail.com>
2020-09-28 16:45:45 -0600

    Add 2 additional hard coded  comments

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import faker from "faker";
 
 const App = () => {
   return (
     <div className="ui container comments">
       <div className="comment">
         <a href="/" className="avatar">
-          <img src="" alt="avatar" />
+          <img src={faker.image.avatar()} alt="avatar" />
         </a>
         <div className="content">
           <a href="/" className="author">
@@ -16,6 +17,34 @@ const App = () => {
             <span className="date">Today at 6:00pm</span>
           </div>
           <div className="text">Nice blog Post!</div>
+        </div>
+      </div>
+      <div className="comment">
+        <a href="/" className="avatar">
+          <img src={faker.image.avatar()} alt="avatar" />
+        </a>
+        <div className="content">
+          <a href="/" className="author">
+            Scottie
+          </a>
+          <div className="metadata">
+            <span className="date">Today at 7:00pm</span>
+          </div>
+          <div className="text">Loved it!</div>
+        </div>
+      </div>
+      <div className="comment">
+        <a href="/" className="avatar">
+          <img src={faker.image.avatar()} alt="avatar" />
+        </a>
+        <div className="content">
+          <a href="/" className="author">
+            TJ
+          </a>
+          <div className="metadata">
+            <span className="date">Today at 8:00pm</span>
+          </div>
+          <div className="text">Wonderful!</div>
         </div>
       </div>
     </div>
