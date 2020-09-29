5ead2922a065fb44a4293864f489f46b581b2512
Dione Wilson <dionew1@gmail.com>
2020-09-29 13:52:30 -0600

    Create Approval Component to wrap comment or other info

--- src/index.js
+++ src/index.js
@@ -2,28 +2,41 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { image, name } from "faker";
 import CommentDetail from "./CommentDetail";
+import ApprovalCard from "./ApprovalCard";
 
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail
-        image={image.avatar()}
-        author={name.firstName()}
-        timeAgo="Today at 4:00PM"
-        text="Great job!"
-      />
-      <CommentDetail
-        image={image.avatar()}
-        author={name.firstName()}
-        timeAgo="Today at 9:23AM"
-        text="Hi Mom!"
-      />
-      <CommentDetail
-        image={image.avatar()}
-        author={name.firstName()}
-        timeAgo="Yesterday at 3:55PM"
-        text="Thank you :)"
-      />
+      <ApprovalCard>
+        <div>
+          <h4>Warning!</h4>
+          Are you sure you want to continue?
+        </div>
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          image={image.avatar()}
+          author={name.firstName()}
+          timeAgo="Today at 4:00PM"
+          text="Great job!"
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          image={image.avatar()}
+          author={name.firstName()}
+          timeAgo="Today at 9:23AM"
+          text="Hi Mom!"
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          image={image.avatar()}
+          author={name.firstName()}
+          timeAgo="Yesterday at 3:55PM"
+          text="Thank you :)"
+        />
+      </ApprovalCard>
     </div>
   );
 };
